afa0688f620a1dc59fc0732d61f44d3f841bb0f0
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2025-08-23 14:58:34 +0300

    message

--- flake.nix
+++ flake.nix
@@ -1,78 +1,67 @@
 {
-  description = "Goal Tracker - Python GTK app";
+  description = "Goal Tracker GTK app in Python";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      python = pkgs.python3;
-    in {
-      packages.${system}.default = python.pkgs.buildPythonApplication {
-        pname = "goal-tracker";
-        version = "0.1.0";
-        src = ./.;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3;
+      in {
+        packages.default = python.pkgs.buildPythonApplication {
+          pname = "goal-tracker";
+          version = "0.1.0";
+          format = "other";
+          src = ./.;
 
-        format = "other";
-        dontUnpack = true;
+          nativeBuildInputs = [
+            pkgs.wrapGAppsHook
+            pkgs.gobject-introspection
+          ];
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp ${./goal-tracker.py} $out/bin/goal-tracker
-          chmod +x $out/bin/goal-tracker
+          propagatedBuildInputs = with python.pkgs; [
+            pygobject3
+          ] ++ [
+            pkgs.gtk3
+            pkgs.gdk-pixbuf
+            pkgs.pango
+            pkgs.atk
+          ];
 
-          # Install desktop entry
-          mkdir -p $out/share/applications
-          cat > $out/share/applications/goal-tracker.desktop <<EOF
-[Desktop Entry]
-Type=Application
-Name=Goal Tracker
-Exec=goal-tracker
-Icon=goal-tracker
-Terminal=false
-Categories=Utility;GTK;
-EOF
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            cp ${./main.py} $out/bin/goal-tracker
+            chmod +x $out/bin/goal-tracker
+            runHook postInstall
+          '';
 
-          # (Optional) install icon if you have goal-tracker.png/svg
-          if [ -f ${./goal-tracker.png} ]; then
-            mkdir -p $out/share/icons/hicolor/256x256/apps
-            cp ${./goal-tracker.png} $out/share/icons/hicolor/256x256/apps/goal-tracker.png
-          fi
-        '';
-
-        nativeBuildInputs = [
-          pkgs.wrapGAppsHook3
-        ];
-
-        propagatedBuildInputs = with python.pkgs; [
-          pygobject3
-        ] ++ [
-          pkgs.gtk3
-          pkgs.gobject-introspection
-        ];
-
-        meta = with pkgs.lib; {
-          description = "GTK-based Goal Tracker";
-          license = licenses.mit;
-          platforms = platforms.linux;
+          meta = with pkgs.lib; {
+            description = "GTK-based goal tracker app";
+            license = licenses.mit;
+            platforms = platforms.linux;
+          };
         };
-      };
 
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/goal-tracker";
-      };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/goal-tracker";
+        };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          python
-          python.pkgs.pygobject3
-          pkgs.gtk3
-          pkgs.gobject-introspection
-        ];
-      };
-    };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            python
+            python.pkgs.pygobject3
+            pkgs.gtk3
+            pkgs.gobject-introspection
+            pkgs.gdk-pixbuf
+            pkgs.pango
+            pkgs.atk
+          ];
+        };
+      });
 }
